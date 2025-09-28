c520f8cdece87ae87cef81c07e54dec68f152e13
Mubarik Osman <zamier372@gmail.com>
2025-09-28 15:16:19 +0300

    fix: correct casing of organizations API route

--- src/app/api/Organizations/route.ts
+++ src/app/api/Organizations/route.ts
@@ -60,3 +60,5 @@ export async function GET(request: Request) {
     return NextResponse.json({ error: `Failed to fetch data: ${error.message}` }, { status: 500 });
   }
 }
+
+////////////////////////////
